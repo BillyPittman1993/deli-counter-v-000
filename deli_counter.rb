4203d32c446d30ef7863061541f44152525fd516
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-06-24 16:42:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,6 +22,7 @@ def now_serving(katz_deli)
   if katz_deli.empty? 
     puts "There is nobody waiting to be served!"
   else 
-    puts "Currently serving katz_deli.pop[0]
+    puts "Currently serving #{name}."
+    katz_deli.pop[0]
   end
 end
